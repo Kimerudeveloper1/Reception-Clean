6a6d4e12f515695169ce404a93a64e6cd38cd217
Kimerudeveloper1 <41114625+Kimerudeveloper1@users.noreply.github.com>
2018-07-11 14:52:30 +0200

    Update node.lua

--- node.lua
+++ node.lua
@@ -283,15 +283,51 @@ local function TileChild(config)
     end
 end
 
-local kenburns_shader = resource.create_shader[[
-    uniform sampler2D Texture;
-    varying vec2 TexCoord;
-    uniform vec4 Color;
-    uniform float x, y, s;
-    void main() {
-        gl_FragColor = texture2D(Texture, TexCoord * vec2(s, s) + vec2(x, y)) * Color;
-    }
-]]
+local shaders={
+   	kenburns_shader=resource.create_shader[[
+	uniform sampler2D Texture;
+        varying vec2 TexCoord;
+        uniform vec4 Color;
+        uniform float x, y, s;
+        void main() {
+            vec2 texcoord = TexCoord * vec2(s, s) + vec2(x, y);
+            vec4 c1 = texture2D(Texture, texcoord);
+            vec4 c2 = texture2D(Texture, texcoord + vec2(0.0016, 0.0000));
+            vec4 c3 = texture2D(Texture, texcoord + vec2(0.0008, 0.0004));
+            vec4 c4 = texture2D(Texture, texcoord + vec2(0.0004, 0.0008));
+            vec4 c5 = texture2D(Texture, texcoord + vec2(0.0000, 0.0016));
+            gl_FragColor = (c5+c4+c3+c2+c1)*0.25 * Color;
+        }
+    ]], 
+    simple = resource.create_shader[[
+        uniform sampler2D Texture;
+        varying vec2 TexCoord;
+        uniform vec4 Color;
+        uniform float x, y, s;
+        void main() {
+            gl_FragColor = texture2D(Texture, TexCoord * vec2(s, s) + vec2(x, y)) * Color;
+        }
+    ]], 
+    progress = resource.create_shader[[
+        uniform sampler2D Texture;
+        varying vec2 TexCoord;
+        uniform float progress_angle;
+        float interp(float x) {
+            return 2.0 * x * x * x - 3.0 * x * x + 1.0;
+        }
+        void main() {
+            vec2 pos = TexCoord;
+            float angle = atan(pos.x - 0.5, pos.y - 0.5);
+            float dist = clamp(distance(pos, vec2(0.5, 0.5)), 0.0, 0.5) * 2.0;
+            float alpha = interp(pow(dist, 8.0));
+            if (angle > progress_angle) {
+                gl_FragColor = vec4(1.0, 1.0, 1.0, alpha);
+            } else {
+                gl_FragColor = vec4(0.5, 0.5, 0.5, alpha);
+            }
+        }
+    ]]
+}
 
 local function Image(config)
     -- config:
@@ -316,9 +352,9 @@ local function Image(config)
 
             local paths = {
                 {from = {x=0.0, y=0.0, s=1.0}, to = {x=0.0, y=0.0, s=0.9}},
-				{from = {x=0.0, y=0.0, s=1.0}, to = {x=0.1, y=0.1, s=0.9}},
+				--[[{from = {x=0.0, y=0.0, s=1.0}, to = {x=0.1, y=0.1, s=0.9}},
 				{from = {x=0.0, y=0.0, s=1.0}, to = {x=0.1, y=0.0, s=0.9}},
-				{from = {x=0.0, y=0.0, s=1.0}, to = {x=0.0, y=0.1, s=0.9}},
+				{from = {x=0.0, y=0.0, s=1.0}, to = {x=0.0, y=0.1, s=0.9}},]]--
             }
 
             local path = paths[math.random(1, #paths)]
