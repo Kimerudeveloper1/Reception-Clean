dbae4ed090421f0375879a3647bae5a1c7affb98
Kimerudeveloper1 <41114625+Kimerudeveloper1@users.noreply.github.com>
2018-07-12 12:43:47 +0200

    Update tile.lua

--- scroller/tile.lua
+++ scroller/tile.lua
@@ -1,7 +1,9 @@
 local api, CHILDS, CONTENTS = ...
 
 local json = require "json"
+local py = require "python"
 local scissors = sys.get_ext "scissors"
+rss = py.import "rss".rss
 
 local font
 local color
@@ -86,7 +88,7 @@ local function draw_scroller(x, y, w, h, parent_config)
             else
                 print "no scroller item. showing blanks"
                 items[#items+1] = {
-                    text = "                      ",
+                    text = " rss()                     ",
                 }
             end
         end
