03b749ed75f21b0466bad22ca43e477f33af79ea
Kimerudeveloper1 <41114625+Kimerudeveloper1@users.noreply.github.com>
2018-07-12 12:40:02 +0200

    Add files via upload

---
+++ scroller/feed.py
@@ -0,0 +1,8 @@
+import feedparser
+
+a=feedparser.parse('https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/2643123')
+
+def rss()
+for post in a.entries:
+	print post.title+ ":"+post.link+"\n"
+
