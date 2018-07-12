84c00fa8d718de9d445ba306b53066fe49c4ec6b
Kimerudeveloper1 <41114625+Kimerudeveloper1@users.noreply.github.com>
2018-07-12 14:02:58 +0200

    Delete feed.py

--- scroller/feed.py
+++
@@ -1,8 +0,0 @@
-import feedparser
-
-a=feedparser.parse('https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/2643123')
-
-def rss():
-for post in a.entries:
-	return post.title+ ":"+post.link+"\n"
-
